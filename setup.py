446f0ce72aeb562b040ee5d7febbba1a6af69a0f
Pierre Vigneras <pivi@amazon.com>
2018-03-30 15:32:32 -0700

    Fix packaging

--- setup.py
+++ setup.py
@@ -111,7 +111,8 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+#     packages=find_packages('src', exclude=['contrib', 'docs', 'tests']),  # Required
+    packages=['src/verlanize'],
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
